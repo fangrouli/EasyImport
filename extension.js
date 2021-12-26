8976f89d16937feb8143a2b9fe3bbd2520f86540
FRL <80878559+fangrouli@users.noreply.github.com>
2021-12-26 17:17:06 +0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -5,6 +5,11 @@ const { spawn } = require('child_process');
 const app = express();
 const port = 4000;
 
+const assert = require('assert');
+const python = require('python-bridge');
+// @ts-ignore
+const py = python();
+
 /**
  * @param {vscode.ExtensionContext} context
  */
@@ -22,45 +27,33 @@ function activate(context) {
 	context.subscriptions.push(disposable1, disposable2);
 }
 
-function FindPathVersion(){
+async function FindPathVersion(){
 	const lib = editor.document.getText(editor.selection);
 
-	app.get('/', (req, res) => {
-		const pyprog = spawn('C:\Users\fangr\AppData\Local\Programs\Python\Python39', ['Side Module.py', lib], { stdio: 'inherit' });
+	py.ex`import os`
 
-		pyprog.stdout.on('data', function(data) {
-			vscode.window.showInformationMessage(data.toString());
-			console.log(data.toString());
-			console.log('Successful.');
+	py.ex`
+		def find(lib_name):
+			try:
+    			lib = __import__(lib_name)
+			except:
+    			return "The library <" + lib_name + "> is not found."
+			path = os.path.dirname(lib.__file__)
+			version = lib.__version__
+			return "Path: " + path + "Version: " + version`;
+
+	try {
+		py`find(${lib})`.then(output => {
+			vscode.window.showInformationMessage(output);
+			console.log('Successful!')
 		});
 
-		pyprog.stderr.on('data', function(data) {
-			vscode.window.showInformationMessage('An error occured.');
-			console.log('An error occured.');
-			console.log('Error code: ' + data.toString());
-		});
-
-		pyprog.stderr.pipe(process.stderr);
-
-		pyprog.on('error', (code) => {
-			console.log('An error occurred: '+ code.message);
-		});
-
-		pyprog.on('close', (code) => {
-			console.log(`child process close all stdio with code ${code}`);
-		});
-	});
-	
-	app.listen(port, () => console.log('Application listening on port 4000!'));
+	} catch(error) {
+		console.log('Error: '+error);
+	};
 }
 
-function deactivate() {
-	console.log("Thank you for using EasyImport, exiting program...");
-
-	let disposable3 = vscode.commands.registerCommand('easyimport.exitEasyImport', function(){
-		vscode.window.showInformationMessage('The extension EasyImport has been terminated!');
-	});
-}
+function deactivate() {}
 
 module.exports = {
 	activate,
